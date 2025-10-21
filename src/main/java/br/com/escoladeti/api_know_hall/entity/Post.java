e19452a6b04f451bb5bd83212105cdf1ce75b359
gabrielAMS012 <gabriel.marassi@gmail.com>
2025-10-20 23:02:29 -0300

    feat: Criando entidade Post com relacionamentos entre tabelas já existentes

---
+++ src/main/java/br/com/escoladeti/api_know_hall/entity/Post.java
@@ -0,0 +1,47 @@
+package br.com.escoladeti.api_know_hall.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "POST")
+public class Post {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  @Column(name = "id", nullable = false)
+  private BigInteger id;
+
+  @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+  @JoinTable(
+    name = "POST_TAGS",
+    joinColumns = @JoinColumn(name = "post_id"),
+    inverseJoinColumns = @JoinColumn(name = "tag_id")
+  )
+  private List<Tag> tags = new ArrayList<>();
+
+  @Column(name = "descricao")
+  private String descricao;
+
+  @Column(name = "titulo")
+  private String titulo;
+
+  @Column(name = "total_up_votes")
+  private Long totalUpVotes;
+
+  @ManyToOne(fetch = FetchType.LAZY, optional = false)
+  @JoinColumn(name = "usuario_id", nullable = false)
+  private Usuario usuario;
+
+}
