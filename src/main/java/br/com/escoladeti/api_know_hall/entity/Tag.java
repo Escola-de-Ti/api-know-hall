b820199e4cd7b74eae8f285759741d215593b9cd
willyantomaz <willyantomaz2015@gmail.com>
2025-10-21 23:20:27 -0300

    fix: mudando para identity a geracao de id

--- src/main/java/br/com/escoladeti/api_know_hall/entity/Tag.java
+++ src/main/java/br/com/escoladeti/api_know_hall/entity/Tag.java
@@ -13,7 +13,7 @@ import java.math.BigInteger;
 public class Tag {
 
   @Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   @Column(name = "id", nullable = false)
   private BigInteger id;
 
