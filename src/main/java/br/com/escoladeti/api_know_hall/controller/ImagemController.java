322ed981bd349a9e6e94a8c49d585b15dad89c50
willyantomaz <willyantomaz2015@gmail.com>
2025-10-29 23:32:33 -0300

    feat: endpoint de upload de imagem

--- src/main/java/br/com/escoladeti/api_know_hall/controller/ImagemController.java
+++ src/main/java/br/com/escoladeti/api_know_hall/controller/ImagemController.java
@@ -1,12 +1,9 @@
 package br.com.escoladeti.api_know_hall.controller;
 
+import br.com.escoladeti.api_know_hall.entity.Imagem;
 import br.com.escoladeti.api_know_hall.service.ImagemService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/storage")
@@ -19,14 +16,9 @@ public class ImagemController {
   }
 
   @PostMapping("/upload")
-  public ResponseEntity<String> upload(@RequestParam("image") MultipartFile image,
-                                       @RequestParam(value = "userId", required = false) String userId) {
-    try {
-      if (userId == null || userId.isBlank()) userId = "test";
-      String url = imagemService.uploadImage(image, userId);
-      return ResponseEntity.ok(url);
-    } catch (Exception e) {
-      return ResponseEntity.status(500).body("Erro: " + e.getMessage());
-    }
+  public ResponseEntity<Imagem> upload(@RequestBody byte[] imagem,
+                                       @RequestParam(value = "imageName", required = false) String imageName) {
+    Imagem url = imagemService.uploadImage(imagem, imageName);
+    return ResponseEntity.ok(url);
   }
 }
