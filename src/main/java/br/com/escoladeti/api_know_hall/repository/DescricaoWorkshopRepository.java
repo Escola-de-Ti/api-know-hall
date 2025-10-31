1f34b4319f0f6ec704a892254686abd277a4619e
gabrielAMS012 <gabriel.marassi@gmail.com>
2025-10-30 21:15:55 -0300

    feat: Criando repositories para entidades Workshop e DescricaoWorkshop para buscar informações relacionadas a essas entidades e fazer o restante da rotina (save, delete etc)

---
+++ src/main/java/br/com/escoladeti/api_know_hall/repository/DescricaoWorkshopRepository.java
@@ -0,0 +1,30 @@
+package br.com.escoladeti.api_know_hall.repository;
+
+import br.com.escoladeti.api_know_hall.entity.workshop.DescricaoWorkshop;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.math.BigInteger;
+import java.util.Optional;
+
+@Repository
+public interface DescricaoWorkshopRepository extends JpaRepository<DescricaoWorkshop, BigInteger> {
+
+  @Query(value = """
+        SELECT dw.*
+        FROM descricao_workshop dw
+        WHERE dw.id_workshop = :workshopId
+        """, nativeQuery = true)
+  Optional<DescricaoWorkshop> findByWorkshopId(@Param("workshopId") Long workshopId);
+
+  @Query(value = """
+        SELECT EXISTS(
+            SELECT 1
+            FROM descricao_workshop dw
+            WHERE dw.id_workshop = :workshopId
+        )
+        """, nativeQuery = true)
+  Boolean existsByWorkshopId(@Param("workshopId") Long workshopId);
+}
