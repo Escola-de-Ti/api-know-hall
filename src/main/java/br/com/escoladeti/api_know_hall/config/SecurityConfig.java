5169640a851e66178405277e9b31c508fecdb60e
willyantomaz <willyantomaz2015@gmail.com>
2025-11-02 23:45:33 -0300

    fix: cors no security

--- src/main/java/br/com/escoladeti/api_know_hall/config/SecurityConfig.java
+++ src/main/java/br/com/escoladeti/api_know_hall/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
@@ -16,9 +17,11 @@ public class SecurityConfig {
   @Bean
   public SecurityFilterChain filterChain(HttpSecurity http, JwtAuthenticationFilter jwtAuthenticationFilter) throws Exception {
     http
+      .cors(Customizer.withDefaults())
       .csrf(AbstractHttpConfigurer::disable)
       .authorizeHttpRequests(authorize -> authorize
         .requestMatchers(HttpMethod.POST, "/api/usuarios", "/api/usuarios/login").permitAll()
+        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
         .anyRequest().authenticated()
       )
       .formLogin(AbstractHttpConfigurer::disable)
