8fcde6a0f91c5e27b8c4ecfae013e2d5422b87c6
gabrielAMS012 <gabriel.marassi@gmail.com>
2025-11-04 22:30:26 -0300

    feat: Adicionando nova rota e DTO de detalhes do post para busca do post aberto no front junto com a lista de comentários

---
+++ src/main/java/br/com/escoladeti/api_know_hall/dto/post/PostDetalhesDTO.java
@@ -0,0 +1,21 @@
+package br.com.escoladeti.api_know_hall.dto.post;
+
+import br.com.escoladeti.api_know_hall.dto.comentario.ComentarioResponseDTO;
+import br.com.escoladeti.api_know_hall.dto.tags.TagResponseDTO;
+
+import java.math.BigInteger;
+import java.sql.Timestamp;
+import java.util.List;
+
+public record PostDetalhesDTO(
+  BigInteger id,
+  BigInteger usuarioId,
+  String usuarioNome,
+  String titulo,
+  String descricao,
+  Long totalUpVotes,
+  List<TagResponseDTO> tags,
+  Timestamp dataCriacao,
+  List<ComentarioResponseDTO> comentarios,
+  boolean hasMoreComentarios
+) {}
