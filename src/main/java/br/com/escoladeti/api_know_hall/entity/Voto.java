ac2c9c4251919102fe9a35dea1ccf907c0d6b7b6
gabrielAMS012 <gabriel.marassi@gmail.com>
2025-11-05 20:14:48 -0300

    feat: Criando estrutura para entidade Voto

---
+++ src/main/java/br/com/escoladeti/api_know_hall/entity/Voto.java
@@ -0,0 +1,51 @@
+package br.com.escoladeti.api_know_hall.entity;
+
+import br.com.escoladeti.api_know_hall.enums.TipoVoto;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.math.BigInteger;
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "votos")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Voto {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private BigInteger id;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "usuario_id", nullable = false)
+  private Usuario usuario;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "post_id")
+  private Post post;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "comentario_id")
+  private Comentario comentario;
+
+  @Enumerated(EnumType.STRING)
+  @Column(nullable = false, length = 20)
+  private TipoVoto tipo;
+
+  @Column(name = "data_criacao", nullable = false, updatable = false)
+  @CreationTimestamp
+  private Timestamp dataCriacao;
+
+  @PrePersist
+  @PreUpdate
+  public void validateVoto() {
+    if ((post == null && comentario == null) || (post != null && comentario != null)) {
+      throw new IllegalStateException("Voto deve ser associado a um Post OU a um Comentário, nunca ambos ou nenhum");
+    }
+  }
+}
---
+++ src/main/java/br/com/escoladeti/api_know_hall/enums/TipoVoto.java
@@ -0,0 +1,6 @@
+package br.com.escoladeti.api_know_hall.enums;
+
+public enum TipoVoto {
+  UP_VOTE,
+  SUPER_VOTE;
+}
